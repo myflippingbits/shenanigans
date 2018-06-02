d90c271f08c33cb648c551453f6f84eb6df76f70
dontflipmybits <38020415+dontflipmybits@users.noreply.github.com>
2018-06-01 23:30:58 -0400

    Code cleanup

--- filescraplogger.js
+++ filescraplogger.js
@@ -1,10 +1,6 @@
 const fs = require('fs');
 fss = fs;
 monitored_directories = ["L:\\Movies", "D:\\Spare Drive Contents\\Movies2", "F:\\Movies"];
-//dirstructure = {};
-//dirstructure_array = [];
-//folderfiles = {};
-
 
 function dirCrawler(directory) {
     fs.readdir(directory, function(err, subDurectories) {
@@ -13,14 +9,11 @@ function dirCrawler(directory) {
             for (i = 0; i < subDurectories.length; i++) {
                 let folder = directory + "\\" + subDurectories[i];
                 folder.replace(/\/\//g, "/");
-                //dirstructure_array.push(folder);
                 let stats = fs.statSync(folder);
                 if (stats.isDirectory()) {
                     fileCrawler(folder);
-                    //console.log("its a directory " + folder);
                 } else
                     fileLogger(folder);
-                //console.log("its a file " + folder);
             }
         }
     });
@@ -59,24 +52,4 @@ function fileLogger(directory, file) {
     console.log(data);
 }
 
-/*
-for (i = 0; i < fulldirectory.length; i++) {
-    let tempfulldirectory = fulldirectory[i];
-    fs.readdir(tempfulldirectory, function(err, files) {
-        if (err) console.log('Error', err);
-        else {
-            folderfiles[tempfulldirectory] = [];
-            for (n = 0; n < files.length; n++) {
-                let temp = tempfulldirectory + "\\" + files[n];
-                temp.replace(/\/\//g, "/");
-                let log = 'drive="' + tempfulldirectory.slice(0, 1) + '" folder="' + tempfulldirectory + '" path="' + temp + '" filename="' + files[n] + '"';
-                folderfiles[tempfulldirectory].push(log); // stuff files from sub directories into a object array or get info and write to file.
-            }
-            console.log('Result', folderfiles[tempfulldirectory]);
-        }
-    });
-}*/
-
-dirCrawler(monitored_directories[0]);
-
-//fileCrawler('L:\\Movies\\J');
+dirCrawler(monitored_directories);
