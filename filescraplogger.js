975cf63302468b96009779e363287de1775d72a2
dontflipmybits <38020415+dontflipmybits@users.noreply.github.com>
2018-06-03 16:43:43 -0400

    V1.0 of filescraplogger.js complete! not only is it working but in correct output format.

--- filescraplogger.js
+++ filescraplogger.js
@@ -1,36 +1,20 @@
 const fs = require('fs');
 fss = fs;
-monitored_directories = ["L:\\Movies", "D:\\Spare Drive Contents\\Movies2", "F:\\Movies"];
+var json = JSON.parse(require('fs').readFileSync('config.json', 'utf8'));
+monitored_directories = json.monitored_directories;
 
 function dirCrawler(directory) {
-    fs.readdir(directory, function(err, subDurectories) {
-        if (err) console.log('Error', err);
+    fs.readdir(directory, function(err, child) {
+        if (err) console.log('Error in dirCrawler', err);
         else {
-            for (i = 0; i < subDurectories.length; i++) {
-                let folder = directory + "\\" + subDurectories[i];
-                folder.replace(/\/\//g, "/");
-                let stats = fs.statSync(folder);
+            for (i = 0; i < child.length; i++) {
+                let filePath = directory + "\\" + child[i];
+                filePath.replace(/\/\//g, "/");
+                let stats = fs.statSync(filePath);
                 if (stats.isDirectory()) {
-                    fileCrawler(folder);
+                    dirCrawler(filePath);
                 } else
-                    fileLogger(folder);
-            }
-        }
-    });
-}
-
-function fileCrawler(fullDirectory) {
-    fs.readdir(fullDirectory, function(err, files) {
-        if (err) console.log('Error', err);
-        else {
-            for (i = 0; i < files.length; i++) {
-                let filePath = fullDirectory + "\\" + files[i];
-                let stats = fss.statSync(filePath);
-                if (stats.isDirectory()) {
-                    fileCrawler(filePath);
-                } else {
-                    fileLogger(fullDirectory, files[i]);
-                }
+                    fileLogger(directory, child[i]);
             }
         }
     });
@@ -45,11 +29,19 @@ function fileLogger(directory, file) {
     data.filePath.replace(/\/\//g, "/");
     data.file = file;
     let stats = fss.statSync(data.filePath);
-    data.size = stats['size'];
-    data.created = stats['ctimeMs'];
-    data.accessed = stats['atimeMs'];
-    data.modified = stats['mtimeMs'];
-    console.log(data);
+    data.size = stats.size;
+    data.created = stats.ctimeMs;
+    data.accessed = stats.atimeMs;
+    data.modified = stats.mtimeMs;
+    logForSplunk(data);
+    //console.log(data);
 }
 
-dirCrawler(monitored_directories);
+function logForSplunk(data) {
+    fs.appendFile('file_entries.txt', JSON.stringify(data) + "\n", function(err) {
+        if (err) throw 'Error writing entry to file' + err;
+    });
+}
+
+for (var i in monitored_directories)
+    dirCrawler(monitored_directories[i]);
