906f4152082512fe8bbe96a9d5ac6cd7eb2e11c1
dontflipmybits <38020415+dontflipmybits@users.noreply.github.com>
2018-06-04 23:09:19 -0400

    Added filescraplogger.js to the cron list

--- scripts/index.js
+++ scripts/index.js
@@ -2,12 +2,17 @@
 const cron = require("node-cron");
 const express = require("express");
 const fs = require("fs");
+const child_process = require("child_process");
 
 app = express();
 
 // schedule tasks to be run on the server   
 cron.schedule("* * * * *", function() {
     console.log("running a task every minute");
+    child_process.exec("node filescraplogger.js", (err, stdout, stderr) => {
+        if (err) throw err;
+        console.log("Indexed files.")
+    });
 });
 
 app.listen(3128);
