6cd13812ad75e19bf0986c3714ed879773cda08a
dontflipmybits <38020415+dontflipmybits@users.noreply.github.com>
2018-06-04 23:10:47 -0400

    Changed cron to once a day.

--- scripts/index.js
+++ scripts/index.js
@@ -7,8 +7,8 @@ const child_process = require("child_process");
 app = express();
 
 // schedule tasks to be run on the server   
-cron.schedule("* * * * *", function() {
-    console.log("running a task every minute");
+cron.schedule("0 20 * * *", function() {
+    console.log("running a task once a day");
     child_process.exec("node filescraplogger.js", (err, stdout, stderr) => {
         if (err) throw err;
         console.log("Indexed files.")
