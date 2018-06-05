2d1cc74c8fb0d3c0dc1e7cb6be6209b22073f5ea
dontflipmybits <38020415+dontflipmybits@users.noreply.github.com>
2018-06-05 18:30:48 -0400

    Preping adding template for cmd tasks

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,30 @@
 // index.js
 const cron = require("node-cron");
 const express = require("express");
-const fs = require("fs");
+//const fs = require("fs");
 const child_process = require("child_process");
 
-app = express();
+var app = express();
 
 // schedule tasks to be run on the server   
 cron.schedule("0 20 * * *", function() {
-    console.log("running a task once a day");
+    console.log("Script a task once a day");
     child_process.exec("node filescraplogger.js", (err, stdout, stderr) => {
         if (err) throw err;
-        console.log("Indexed files.")
+        else if (stderr) throw stderr;
+        console.log("Indexed files.");
+    });
+});
+
+cron.schedule("0 20 * * *", function() {
+    console.log("Command a task once a day");
+    /*child_process.execFile(file, args, options, function(error, stdout, stderr) {
+        // command output is in stdout
+    });*/
+    child_process.execFile("node filescraplogger.js", (err, stdout, stderr) => {
+        if (err) throw err;
+        else if (stderr) throw stderr;
+        console.log("Indexed files.");
     });
 });
 
